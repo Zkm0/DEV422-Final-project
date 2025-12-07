0028709efcbbf1335eae31621522fd10f497e8d0
Zoe Mukaba <zoemk07@gmail.com>
2025-12-07 01:07:35 -0800

    Added Player model and initial PlayersController

---
+++ FantasySportsTeamManagement/PlayerManagementService/Controllers/PlayersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlayerManagementService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        // GET: api/players
+        // For now this just returns a placeholder message
+        // We'll hook this up to the database later
+        [HttpGet]
+        public IActionResult GetPlayers()
+        {
+            return Ok("Player list placeholder from PlayerManagementService");
+        }
+    }
+}
