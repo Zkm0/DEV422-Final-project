2b2a7124f1b8dc89c5e147d65ab5f67a5a06d2ff
Colin Jones <colin@texis.com>
2025-12-10 14:07:43 -0800

    First commit of Performance manager

---
+++ FantasySportsTeamManagement/PerformanceService/Program.cs
@@ -0,0 +1,17 @@
+using PerformanceService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR().AddAzureSignalR("tcp:nzc-fantasyteam-sql.database.windows.net,1433;" +
+    "Initial Catalog=PerformanceDB;Persist Security Info=False;User ID=nathanzoecolin;Password=Test1234;" +
+    "MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+var app = builder.Build();
+
+app.MapHub<RealTimePerformance>("/performance");
+
+app.MapFallbackToFile("performance.html");
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
