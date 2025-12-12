f5b15b4c48849f7d5d1ade21c017eee32a204657
Zoe Mukaba <zoemk07@gmail.com>
2025-12-11 22:25:21 -0800

    PlayerService: add TeamService URL config and update Program setup

--- FantasySportsTeamManagement/PlayerManagementService/Program.cs
+++ FantasySportsTeamManagement/PlayerManagementService/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PlayerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PlayerDB")));
 
+builder.Services.AddHttpClient();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
